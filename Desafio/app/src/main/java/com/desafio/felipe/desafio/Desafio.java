897e45348565c4b3b92b3f608d164f05f155e77f
Felipemv <felipevitor@gec.inatel.br>
2017-12-13 23:05:46 -0200

    Projeto Criado

---
+++ Desafio/app/src/main/java/com/desafio/felipe/desafio/Desafio.java
@@ -0,0 +1,13 @@
+package com.desafio.felipe.desafio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Desafio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desafio);
+    }
+}
